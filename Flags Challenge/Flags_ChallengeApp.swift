901b395ab2cb0bf7b4817400c78263adb916962a
Adithyan N A <itIos04@manappuram.com>
2025-08-05 11:27:12 +0530

    Initial Commit

---
+++ Flags Challenge/Flags_ChallengeApp.swift
@@ -0,0 +1,20 @@
+//
+//  Flags_ChallengeApp.swift
+//  Flags Challenge
+//
+//  Created by adithyan na on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Flags_ChallengeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
